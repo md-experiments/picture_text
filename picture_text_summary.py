eb810b41ec4b71a85b5f2fa85c248332d32c4c24
Mihail <md@mihails-mbp.home>
2020-10-03 15:23:00 +0100

    use cluster similarity as color

--- picture_text_summary.py
+++ picture_text_summary.py
@@ -18,10 +18,11 @@ def cluster_summary_simple(clust_txt, model, clust_emb = [], top_n=1, text_if_em
         clust_avg=np.mean(clust_emb, axis=0, keepdims=True)
         df['cluster_rank']=0.5*(1 + cosine_similarity(clust_emb,clust_avg))
         df1=df.sort_values('cluster_rank',ascending=False).copy()
+        centroid_similarity = df['cluster_rank'].mean()
         if top_n==1:
-            return df1.iloc[0].titles
+            return str(df1.iloc[0].titles).title(), centroid_similarity
         else:
-            return list(df1.head(top_n).titles.values)
+            return list(df1.head(top_n).titles.values), centroid_similarity
 
 
 def unroll_tree_map(X, method='single', depth=3, nr_splits=3,min_size=0.1,max_extension=1):
@@ -29,18 +30,7 @@ def unroll_tree_map(X, method='single', depth=3, nr_splits=3,min_size=0.1,max_ex
     #clust_nm = 'all'
     clust_idx = -1
     len_x = len(X)
-    res_d={clust_idx:
-        {
-        'cluster_id': clust_idx,
-        'cluster_parent': 'all',
-        'cluster_members': [],
-        'cluster_table': {},
-        'cluster_size': len(X),
-        }
-          }
-    df_res = pd.DataFrame(res_d).T
-    #col_nm = ['value', 'color', 'parent_idx', 'id_idx','parent_nm', \
-    #         'id_nm', 'members_ids', 'members_txt', 'members_embeddings']
+    df_res = pd.DataFrame([],columns=['cluster_id', 'cluster_parent', 'cluster_members', 'cluster_table', 'cluster_size'])
 
     z=fastcluster.linkage(X, method=method)
     hac = HAC(z, parent = clust_idx)
@@ -61,7 +51,7 @@ def unroll_tree_map(X, method='single', depth=3, nr_splits=3,min_size=0.1,max_ex
         if depth<1:
             go = False
     
-    df_res['color']=df_res['cluster_size']/len_x
+    #df_res['color']=df_res['cluster_size']/len_x
     col_nm={'cluster_size':'value','cluster_id':'id','cluster_parent':'parent'}
     df_res=df_res.rename(columns=col_nm)
     return df_res
